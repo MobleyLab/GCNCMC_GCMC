810c96521a199b754591b067b88c1183f1237536
yunhuige <yunhui.ge@gmail.com>
2022-05-02 15:40:46 -0700

    input files

---
+++ input/gcmc/5i1q/new_start/prod1/restart.py
@@ -0,0 +1,83 @@
+import sys, os
+from simtk.openmm import *
+from simtk.openmm.app import *
+from simtk.unit import *
+from openmmtools.integrators import BAOABIntegrator
+from sys import stdout
+import numpy as np
+import mdtraj
+import grand
+
+# Load PDB
+pdb = PDBFile('5i1q_ghosts_gcmc.pdb')
+
+# Load restart file
+rst7 = AmberInpcrdFile('5i1q_prod5_gcmc.rst7')
+
+# Shouldn't need to add ghosts as these can just be read in from before (all frames contained below)
+ghosts = grand.utils.read_ghosts_from_file('5i1q_prod5_gcmc.txt')
+
+ff = ForceField('amber14/protein.ff14SB.xml', 'amber14/tip3p.xml', 'LIG_test.xml')
+system = ff.createSystem(pdb.topology,
+                         nonbondedMethod=PME,
+                         nonbondedCutoff=10.0*angstroms,
+                         switchDistance=8.0*angstroms,
+                         constraints=HBonds)
+
+ref_atoms = [{'name': 'CA', 'resname': 'GLU', 'resid': '51', 'chain': 0},{'name': 'CA', 'resname': 'ASN', 'resid': '83', 'chain': 0}]
+
+
+# Define GCMC Sampler
+gcmc_mover = grand.samplers.StandardGCMCSphereSampler(system=system,
+                                                      topology=pdb.topology,
+                                                      temperature=277*kelvin,
+                                                      referenceAtoms=ref_atoms,
+                                                      sphereRadius=7.0*angstroms,
+                                                      log='5i1q-prod1.log',
+                                                      excessChemicalPotential=-6.34*kilocalorie_per_mole,
+                                                      standardVolume=29.823*angstroms**3,
+                                                      ghostFile='5i1q-prod1.txt',
+                                                      dcd='5i1q-prod1.dcd',
+                                                      rst='5i1q-prod1.rst7',
+                                                      overwrite=True)
+
+
+# Define integrator
+integrator = BAOABIntegrator(277*kelvin, 1.0/picosecond, 0.002*picosecond)
+
+# Define platform and set precision
+platform = Platform.getPlatformByName('CUDA')
+platform.setPropertyDefaultValue('Precision', 'mixed')
+
+# Create simulation object
+simulation = Simulation(pdb.topology, system, integrator, platform)
+
+# Set positions, velocities and box vectors
+simulation.context.setPositions(pdb.positions)
+#simulation.context.setVelocitiesToTemperature(277*kelvin)
+simulation.context.setPositions(rst7.getPositions())
+simulation.context.setVelocities(rst7.getVelocities())
+simulation.context.setPeriodicBoxVectors(*pdb.topology.getPeriodicBoxVectors())
+
+# Prepare the GCMC sphere
+gcmc_mover.initialise(simulation.context, ghosts[-1])
+
+
+# Add StateDataReporter for production
+simulation.reporters.append(StateDataReporter(stdout,
+                                              1000,
+                                              step=True,
+                                              potentialEnergy=True,
+                                              temperature=True,
+                                              volume=True,
+                                              speed=True))
+
+# Run GCMC/MD equilibration (500k GCMC moves over 10 ns - 50 moves every 1 ps)
+for i in range(2500):
+    simulation.step(500)
+    gcmc_mover.move(simulation.context, 50)
+    # Write out a frame every 20 ps
+#    if (i+1) % 20 == 0:
+    gcmc_mover.report(simulation)
+
+
